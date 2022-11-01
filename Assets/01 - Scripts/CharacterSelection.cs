933cf80d8a0c408fb01258660fb94aeee3f26079
globulous <r.pitot@laposte.net>
2022-11-02 00:40:45 +0100

    correctif

--- Assets/01 - Scripts/CharacterSelection.cs
+++ Assets/01 - Scripts/CharacterSelection.cs
@@ -1,103 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class CharacterSelection : MonoBehaviour
 {
+    public List<GameObject> Player1 = new List<GameObject>();
+    public List<GameObject> Player2 = new List<GameObject>();
 
-
-
-
-    public GameObject P11;
-    public GameObject P12;
-    public GameObject P13;
-    public GameObject P14;
-    
-    public GameObject P21;
-    public GameObject P22;
-    public GameObject P23;
-    public GameObject P24;
-
-    // Start is called before the first frame update
-    void Start()
+    public void P1Apparence1Clicked()
     {
-
+        Player1[0].SetActive(true);
+        Player1[1].SetActive(false);
+        Player1[2].SetActive(false);
+        Player1[3].SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void P1Apparence2Clicked()
     {
-        
+        Player1[0].SetActive(false);
+        Player1[1].SetActive(true);
+        Player1[2].SetActive(false);
+        Player1[3].SetActive(false);
     }
 
-    public void P11Clicked()
+    public void P1Apparence3Clicked()
     {
-        P11.SetActive(true);
-        P12.SetActive(false);
-        P13.SetActive(false);
-        P14.SetActive(false);
+        Player1[0].SetActive(false);
+        Player1[1].SetActive(false);
+        Player1[2].SetActive(true);
+        Player1[3].SetActive(false);
     }
-    
-    public void P12Clicked()
+
+    public void P1Apparence4Clicked()
     {
-        P11.SetActive(false);
-        P12.SetActive(true);
-        P13.SetActive(false);
-        P14.SetActive(false);
-    }
-    
-    public void P13Clicked()
-    {
-        P11.SetActive(false);
-        P12.SetActive(false);
-        P13.SetActive(true);
-        P14.SetActive(false);
-    }
-    
-    public void P14Clicked()
-    {
-        P11.SetActive(false);
-        P12.SetActive(false);
-        P13.SetActive(false);
-        P14.SetActive(true);
+        Player1[0].SetActive(false);
+        Player1[1].SetActive(false);
+        Player1[2].SetActive(false);
+        Player1[3].SetActive(true);
     }
 
 
 
-    public void P21Clicked()
+    public void P2Apparence1Clicked()
     {
-        P21.SetActive(true);
-        P22.SetActive(false);
-        P23.SetActive(false);
-        P24.SetActive(false);
+        Player2[0].SetActive(true);
+        Player2[1].SetActive(false);
+        Player2[2].SetActive(false);
+        Player2[3].SetActive(false);
     }
 
-    public void P22Clicked()
+    public void P2Apparence2Clicked()
     {
-        P21.SetActive(false);
-        P22.SetActive(true);
-        P23.SetActive(false);
-        P24.SetActive(false);
+        Player2[0].SetActive(false);
+        Player2[1].SetActive(true);
+        Player2[2].SetActive(false);
+        Player2[3].SetActive(false);
     }
 
-    public void P23Clicked()
+    public void P2Apparence3Clicked()
     {
-        P21.SetActive(false);
-        P22.SetActive(false);
-        P23.SetActive(true);
-        P24.SetActive(false);
+        Player2[0].SetActive(false);
+        Player2[1].SetActive(false);
+        Player2[2].SetActive(true);
+        Player2[3].SetActive(false);
     }
 
-    public void P24Clicked()
+    public void P2Apparence4Clicked()
     {
-        P21.SetActive(false);
-        P22.SetActive(false);
-        P23.SetActive(false);
-        P24.SetActive(true);
+        Player2[0].SetActive(false);
+        Player2[1].SetActive(false);
+        Player2[2].SetActive(false);
+        Player2[3].SetActive(true);
     }
 
     public void LaunchGame()
     {
-        //SceneManager.LoadScene(1);
+        //SceneManager.LoadScene(X);
     }
 }
