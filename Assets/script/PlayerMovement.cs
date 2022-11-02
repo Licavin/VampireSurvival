501931e9f3d16837d972426d717c1e5c61861b48
LenyNICOLAS <lnicolas@gaming.tech>
2022-11-02 12:21:10 +0100

    ajout deplacement verticaux

--- Assets/script/PlayerMovement.cs
+++ Assets/script/PlayerMovement.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Windows;
+using Input = UnityEngine.Input;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -11,6 +13,8 @@ public class PlayerMovement : MonoBehaviour
     public Animator animator;
     public SpriteRenderer spriteRenderer;
 
+    public Vector2 speed = new Vector2(20, 20);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,22 +24,41 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(speed.x * inputX, speed.y * inputY, 0);
+
+        movement *= Time.deltaTime;
+
+        transform.Translate(movement);
+
+
+
+
+
+
         float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
 
-        MovePlayer(horizontalMovement);
+        HorizontalMovePlayer(horizontalMovement);
 
         Flip(rb.velocity.x);
 
         float characterVelocity = Mathf.Abs(rb.velocity.x);
         animator.SetFloat("Speed", characterVelocity);
 
+
     }
 
-    void MovePlayer(float _horizontalMovement)
+    
+    void HorizontalMovePlayer(float _horizontalMovement)
     {
         Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
     }
+
+
+
 
     void Flip(float _velocity)
     {
