d129f9599b62d30628de9580d01919faa6b2b0f1
LenyNICOLAS <lnicolas@gaming.tech>
2022-11-02 17:10:57 +0100

    tir player

--- Assets/script/ProjectileMove.cs
+++ Assets/script/ProjectileMove.cs
@@ -7,32 +7,38 @@ public class ProjectileMove : MonoBehaviour
 {
 
     [SerializeField] private GameObject target;
-    [SerializeField] public float speed = 1.5f;
+    public float speed = 1.5f;
 
-    //public float speed = 12f;
     Transform myTransform;
-    //public Transform Target;
    
 
     // Start is called before the first frame update
     void Start()
     {
        
-        myTransform = GetComponent<Transform>();
+        //myTransform = GetComponent<Transform>();
         
     }
 
     // Update is called once per frame
     void Update()
-    {
-        //myTransform.Translate(Vector3.up*Time.deltaTime*speed);
-
-        //myTransform.Translate(new Vector3(10f, 10f, 10f) * Time.deltaTime, target.transform);
-        //float step = Time.deltaTime * speed;
-        //myTransform.transform.position = Vector3.MoveTowards(myTransform.transform.position, Target.position, step);
-
+    {   
+        var enemies = FindObjectsOfType<AEnemy>();
+        float minDist = float.MaxValue;
+        GameObject closestEnemy=this.gameObject;
+        foreach (var enemy in enemies)
+        {
+            if((enemy.transform.position - transform.position).magnitude<minDist)
+            {
+                minDist = enemy.transform.position.magnitude;
+                closestEnemy = enemy.gameObject;
+            }
+        }
+        target.transform.position = closestEnemy.transform.position;
         transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
         transform.up = target.transform.position - transform.position;
+
+
 
 
         Destroy(this.gameObject, 3f);
