4afaa3967edfb61e5c7c6835598589b43c685f77
Romain Pitot <romain.pitot@e-artsup.net>
2022-11-02 17:15:24 +0100

    UI Select champ

--- Assets/MenuScrip.cs
+++ Assets/MenuScrip.cs
@@ -6,24 +6,45 @@ public class MenuScrip : MonoBehaviour
 {
     public GameObject joinIPPanel;
     public GameObject hostIPPanel;
+    public GameObject characterCanvas;
+    public GameObject ipCanvas;
+    public GameObject MenuCanvas;
+    public GameObject ErrorCanvas;
+
+    bool host;
+    bool join;
+    bool wrongIP;
 
 
-
-
+    public void Update()
+    {
+        if(wrongIP == true)
+        {
+            ErrorCanvas.SetActive(true);
+        }
+        else
+        {
+            ErrorCanvas.SetActive(false);
+        }
+    }
     public void JoinIP()
     {
+        join = true;
         hostIPPanel.SetActive(false);
         joinIPPanel.SetActive(true);
     }
 
     public void HostIP()
     {
+        host = true;
         hostIPPanel.SetActive(true);
         joinIPPanel.SetActive(false);
     }
 
     public void IPnextStep()
     {
+        characterCanvas.SetActive(true);
+        ipCanvas.SetActive(false);
         Debug.Log("continue");
     }
 }
