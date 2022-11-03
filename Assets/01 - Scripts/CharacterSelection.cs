78bd52f7ba6c3f335f8551bd2408671541436446
Romain Pitot <romain.pitot@e-artsup.net>
2022-11-03 09:28:48 +0100

    Menu Sélection perso finalisé

--- Assets/01 - Scripts/CharacterSelection.cs
+++ Assets/01 - Scripts/CharacterSelection.cs
@@ -9,70 +9,39 @@ public class CharacterSelection : MonoBehaviour
     public List<GameObject> Player1 = new List<GameObject>();
     public List<GameObject> Player2 = new List<GameObject>();
 
+    public List<GameObject> SelectionOverlay = new List<GameObject>();
+
+
     public void P1Apparence1Clicked()
     {
+        SelectionOverlay[0].SetActive(true);
+        SelectionOverlay[1].SetActive(false);
         Player1[0].SetActive(true);
         Player1[1].SetActive(false);
-        Player1[2].SetActive(false);
-        Player1[3].SetActive(false);
     }
 
     public void P1Apparence2Clicked()
     {
+        SelectionOverlay[0].SetActive(false);
+        SelectionOverlay[1].SetActive(true);
         Player1[0].SetActive(false);
         Player1[1].SetActive(true);
-        Player1[2].SetActive(false);
-        Player1[3].SetActive(false);
     }
-
-    public void P1Apparence3Clicked()
-    {
-        Player1[0].SetActive(false);
-        Player1[1].SetActive(false);
-        Player1[2].SetActive(true);
-        Player1[3].SetActive(false);
-    }
-
-    public void P1Apparence4Clicked()
-    {
-        Player1[0].SetActive(false);
-        Player1[1].SetActive(false);
-        Player1[2].SetActive(false);
-        Player1[3].SetActive(true);
-    }
-
-
 
     public void P2Apparence1Clicked()
     {
+        SelectionOverlay[2].SetActive(true);
+        SelectionOverlay[3].SetActive(false);
         Player2[0].SetActive(true);
         Player2[1].SetActive(false);
-        Player2[2].SetActive(false);
-        Player2[3].SetActive(false);
     }
 
     public void P2Apparence2Clicked()
     {
+        SelectionOverlay[2].SetActive(false);
+        SelectionOverlay[3].SetActive(true);
         Player2[0].SetActive(false);
         Player2[1].SetActive(true);
-        Player2[2].SetActive(false);
-        Player2[3].SetActive(false);
-    }
-
-    public void P2Apparence3Clicked()
-    {
-        Player2[0].SetActive(false);
-        Player2[1].SetActive(false);
-        Player2[2].SetActive(true);
-        Player2[3].SetActive(false);
-    }
-
-    public void P2Apparence4Clicked()
-    {
-        Player2[0].SetActive(false);
-        Player2[1].SetActive(false);
-        Player2[2].SetActive(false);
-        Player2[3].SetActive(true);
     }
 
     public void LaunchGame()
